31c60f3ba6423e71538ab6f6278efe8b3a4ee036
devmaca <m.a.c.a-964@hotmail.com>
2020-09-01 21:56:09 -0400

    mostrar contenido tipo video y docu

--- sesiones/sesiondoce.js
+++ sesiones/sesiondoce.js
@@ -3,7 +3,6 @@ module.exports = function(req,res,next){
 	console.log(req.session.usuario)
 	if(!req.session.usuario){ //si no existe valor no hay usuario
 		console.log('Usuario no encontrado');
-
 		res.redirect("/")// vuelve a ruta login
 
 	}
